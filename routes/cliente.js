57a7cbfd8215cf257fc3db2a399ab9d8afd17a17
Christian Farias <kurisu615@gmail.com>
2018-12-19 17:03:46 -0300

    Fixed: ingreso de cliente (parsear rut sin definir)

--- routes/cliente.js
+++ routes/cliente.js
@@ -8,7 +8,7 @@ const Cliente = require('../models/index').Cliente;
 // Muestra una lista con todos los clientes.
 router.get('/', (req, res) => controlador.listar(req, res, Cliente));
 
-router.get('/contar', (req, res) => controlador.contar(req,res, Cliente))
+router.get('/contar', (req, res) => controlador.contar(req, res, Cliente))
 
 // Realiza una consulta a la tabla Cliente y retorna los resultados.
 router.get('/search', (req, res) => controlador.buscarClientes(req, res));
@@ -17,9 +17,15 @@ router.get('/:id', (req, res) => controlador.getById(req, res, Cliente));
 
 
 // Crea un Cliente.
-router.post('/', (req, res) => {req.body.rut = req.body.rut.replace(/\./g,'')
-                                                .replace(/-/g,'').toLowerCase();;
-                                controlador.crear(req, res, Cliente);});
+router.post('/', (req, res) => {
+    // Parsear rut solo si fue ingresado.
+    if (req.body.rut)
+        req.body.rut = req.body.rut.replace(/\./g, '')
+            .replace(/-/g, '').toLowerCase();;
+
+    // El modelo se encarga de validar el rut.
+    controlador.crear(req, res, Cliente);
+});
 
 //Elimina un Cliente
 router.delete('/:id', (req, res) => controlador.eliminar(req, res, Cliente));
